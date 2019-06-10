6ab5972249a85060e0958060c97118a88612a592
Jocelyn Bergdorff <joce.roseb@gmail.com>
2019-06-10 19:17:03 -0400

    adds Renderer method usng Graphics

--- src/FlappyBird.java
+++ src/FlappyBird.java
@@ -1,5 +1,7 @@
 // package flappyBird;
 
+// Import Graphics;
+import java.awt.Graphics;
 // Be sure to import JFrame(the window where the game will be displayed)
 import javax.swing.JFrame;
 
@@ -13,11 +15,19 @@ public class FlappyBird {
     // Set JFrame info inside the FlappyBird Function
     public FlappyBird() {
         JFrame jframe = new JFrame();
+
+        jframe.setResizeable(false);
         jframe.setSize(WIDTH, HEIGHT);
         jframe.setVisible(true);
+        // terminate program on close button
+        jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
     public static void main(String[] args) {
         flappyBird = new FlappyBird();
         
+    }
+
+    public void repaint(Graphics g) {
+        // TODO: Auto-generate method
     }
 }
