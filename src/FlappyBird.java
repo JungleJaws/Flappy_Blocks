c0cb69a5dbbd4e7a68ae51f4f58446e483ed3fd9
Jocelyn Bergdorff <joce.roseb@gmail.com>
2019-06-10 20:56:00 -0400

    adds add column function and draws bird

--- src/FlappyBird.java
+++ src/FlappyBird.java
@@ -2,14 +2,17 @@
 
 // Import Graphics;
 import java.awt.Graphics;
+import java.awt.Rectangle;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseListener;
 
 // Be sure to import JFrame(the window where the game will be displayed)
 import javax.swing.JFrame;
 // Import Timer
 import javax.swing.Timer;
 
-public class FlappyBird implements ActionListener{
+public class FlappyBird implements ActionListener, MouseListener, KeyListener{
     // create new instance of FlappyBird called flappyBird
     public static FlappyBird flappyBird;
     
@@ -19,20 +22,52 @@ public class FlappyBird implements ActionListener{
     // Bring in the Renderer Method
     public Renderer renderer;
     // Set JFrame info inside the FlappyBird Function
+
+    public Rectangle bird;
+    public ArrayList<Rectangle> columns;
     public FlappyBird() {
         JFrame jframe = new JFrame();
         Timer timer = new Timer(20, this);
 
         renderer = new Renderer();
+        rand = new Random();
 
         jframe.add(renderer);
+        // Set Tiltle
+        jframe.setTitle("Flappy Bird");
         // jframe.setResizeable(false); -- causing error on load, comeback to.
         jframe.setSize(WIDTH, HEIGHT);
+        // add event listeners
+        jframe.addMouseListener(this);
+        jframe.addKeyListener(this);
         jframe.setVisible(true);
         // terminate program on close button
         jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        bird = new Rectangle(WIDTH / 2 - 10, HEIGHT / 2 - 10, 20, 20);
+        columns = new ArrayList<Rectangle>();
+
+        addColumn(true);
+        addColumn(true);
+        addColumn(true);
+        addColumn(true);
+
+        timer.start();
     }
 
+    public void addColumn(boolean start) {
+        int space = 300;
+        int width = 100;
+        int height = 50 + rand.nextInt(300);
+
+        if (start) {
+            columns.add(new Rectangle(WIDTH + width + columns.size() * 300, HEIGHT - height - 120, width, height));
+            columns.add(new Rectangle(WIDTH + width + (columns.size() - 1) * 300, 0, width, HEIGHT - height - space));
+        } else {
+            columns.add(new Rectangle(columns.get(columns.size() - 1).x + 600, HEIGHT - height - 120, width, height));
+            columns.add(new Rectangle(columns.get(columns.size() - 1 ).x, 0, width, HEIGHT - height - space));
+        }
+    }
     @Override
     public void actionPerformed(ActionEvent e) {
         renderer.repaint();
